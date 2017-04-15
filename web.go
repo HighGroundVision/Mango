d85a97e88eae6e8eb8f10c2b240eb393324623f7
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2017-04-14 22:43:57 -0400

    lets try this again...

--- web.go
+++ web.go
@@ -443,7 +443,6 @@ func v1ParseLog(w http.ResponseWriter, r *http.Request) {
 }
 
 func everythingElse(w http.ResponseWriter, r *http.Request) {
-    // http.Redirect(w, r, "welcome.html", 301)
 	http.Error(w, "Welcome to Mango, please post a replay to '/v1/parse' to get started.", http.StatusNotFound)
 	return
 }
